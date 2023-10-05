0af48957fa900a6488bc4c6e3a351efe15096cb0
Jose Estuardo Castellon Moscoso <110071197+jecm72@users.noreply.github.com>
2023-10-05 17:42:35 -0600

    Add files via upload

---
+++ src/main/java/com/example/SpringSecurityJwtApplication.java
@@ -0,0 +1,76 @@
+package com.example;
+
+import com.example.models.ERole;
+import com.example.models.RoleEntity;
+import com.example.models.UserEntity;
+import com.example.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
+
+@SpringBootApplication
+public class SpringSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtApplication.class, args);
+	}
+
+	@Autowired
+	PasswordEncoder passwordEncoder;
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Bean
+	CommandLineRunner init(){
+		return args -> {
+
+			UserEntity userEntity = UserEntity.builder()
+					.email("info@netronica.net")
+					.username("Estuardo Castellon")
+					.password(passwordEncoder.encode("Romel#2022"))
+					.roles(Set.of(RoleEntity.builder()
+							.name(ERole.valueOf(ERole.ADMIN.name()))
+							.build()))
+					.build();
+
+			UserEntity userEntity2 = UserEntity.builder()
+					.email("marixa.contreras@gmail.com")
+					.username("Marixa Contreras")
+					.password(passwordEncoder.encode("Maria$2022"))
+					.roles(Set.of(RoleEntity.builder()
+							.name(ERole.valueOf(ERole.USER.name()))
+							.build()))
+					.build();
+
+			UserEntity userEntity3 = UserEntity.builder()
+					.email("andrea.perez@mail.com")
+					.username("Andrea Perez")
+					.password(passwordEncoder.encode("1234"))
+					.roles(Set.of(RoleEntity.builder()
+							.name(ERole.valueOf(ERole.INVITED.name()))
+							.build()))
+					.build();
+
+			 UserEntity userEntity4 = UserEntity.builder()
+					.email("rudy.perez@mail.com")
+					.username("Rudy Perez")
+					.password(passwordEncoder.encode("909090"))
+					.roles(Set.of(RoleEntity.builder()
+							.name(ERole.valueOf(ERole.INVITED.name()))
+							.build()))
+					.build();
+
+			userRepository.save(userEntity);
+			userRepository.save(userEntity2);
+			userRepository.save(userEntity3);
+			userRepository.save(userEntity4);
+		};
+	}
+
+}
